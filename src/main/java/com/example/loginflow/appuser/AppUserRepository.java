589d18080c7bed31ae307acd5d80f6a0868f5abc
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-05-04 16:12:32 -0500

    Minor fix on repository

--- src/main/java/com/example/loginflow/appuser/AppUserRepository.java
+++ src/main/java/com/example/loginflow/appuser/AppUserRepository.java
@@ -1,12 +1,12 @@
 package com.example.loginflow.appuser;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByEmail(String email);
 }
