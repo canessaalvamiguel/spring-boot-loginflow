061ed891e9ecdd81d8653296d2add4bc57816337
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-05-04 13:50:55 -0500

    Implementing login service and model

---
+++ src/main/java/com/example/loginflow/appuser/AppUserRepository.java
@@ -0,0 +1,12 @@
+package com.example.loginflow.appuser;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+    Optional<AppUser> findByEmail(String email);
+}
---
+++ src/main/java/com/example/loginflow/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.loginflow.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
