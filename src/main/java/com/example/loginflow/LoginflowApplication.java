92cc097ca1a2d334ef69586a1c23d780afb6ff9b
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-05-04 11:08:44 -0500

    Initial commit

---
+++ src/main/java/com/example/loginflow/LoginflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loginflow/LoginflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
